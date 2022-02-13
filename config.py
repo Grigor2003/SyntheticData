805cc858e9734fc3ca650e3749a0c7dda236badf
narek <narek.ginoyan.1000@gmail.com>
2022-02-13 14:56:20 +0400

    Beta 2.1.1

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # Add '/' at the end of dirs' PATHs
-result_path = "Result/"  # Enter Result's folder PATH
-bg_path = "Bg/"  # Enter background pictures' folder PATH
-obj_path = "Obj/"  # Enter objects' folder PATH
+result_path = "Result/"
+bg_path = "Bg/"
+obj_path = "Obj/"
 # Parameters
-ignore_symbol = "!!!!"  # You can specify symbol for skipping the object if it is in object's file name
-merge = True  # If False, creates separate folders for data and labels
-bg_packages = False  # True for separate folders for bg_names
-out_of_bounds = False  # True for allowing objects be out of bounds
-count = 3  # How many times the object will be placed on the bg
-distribution = "linear"  # linear or gaussian
-scale_rate = 1  # Scale, two element tuple for range, list for particular rates
-blur_chance = 1  # Chance of blurring the object
-blur_rate = 0  # Blur, two element tuple for range, list for particular rates
-noise_rate = 0  # Noise, two element tuple for range, list for particular rates
+ignore_symbol = "!!!!"
+merge = True
+bg_packages = False
+out_of_bounds = False
+count = 3
+distribution = "linear"
+scale_rate = 1
+blur_chance = 1
+blur_rate = 0
+noise_rate = 0
